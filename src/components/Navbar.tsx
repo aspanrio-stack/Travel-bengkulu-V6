bfbbf24b2828365578524f58007f3af27b69884c
aspanrio-stack <aspan.rio@gmail.com>
2026-04-13 05:21:02 +0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const navLinks = [
   {
-    label: 'Layanan',
+    label: 'Jurusan',
     children: [
       { href: '/travel-bengkulu-palembang', label: 'Bengkulu ↔ Palembang' },
       { href: '/travel-bengkulu-jambi', label: 'Bengkulu ↔ Jambi' },
