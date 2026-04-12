754435605a69910139743b39a5b9c09fa62eafe9
aspanrio-stack <aspan.rio@gmail.com>
2026-04-12 07:49:16 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,3 +84,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   );
 }
+<script
+  src="https://app.sandbox.midtrans.com/snap/snap.js"
+  data-client-key={process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY}
+  strategy="beforeInteractive"
+/>
