76f0fe53a54be73141cedf368db0420226fb1606
aspanrio-stack <aspan.rio@gmail.com>
2026-04-26 16:16:06 +0700

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,6 +5,12 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      // Tambahkan disallow di bawah ini untuk memblokir URL penyebab error di GSC
+      disallow: [
+        '/feeds/',      // Memblokir folder feed
+        '/*?m=1',       // Memblokir parameter feed/mobile sisa sistem lama
+        '/*?alt=rss',   // Memblokir parameter RSS
+      ],
     },
     sitemap: 'https://bengkulutravel.com/sitemap.xml',
   };
