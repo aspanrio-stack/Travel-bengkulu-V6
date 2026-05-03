1b04b295a28f1389a4c97b4ff909325cc020f026
aspanrio-stack <aspan.rio@gmail.com>
2026-05-03 17:47:41 +0700

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.bengkulutravel.com';
+  const baseUrl = 'https://bengkulutravel.com';
   const lastModified = new Date();
 
   return [
